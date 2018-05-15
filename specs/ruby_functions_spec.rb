c1fcb96fb75e0caacfb9a4953a57bcdb8e9fe46b
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-05-15 19:03:41 +0100

    Refactor for better variables naming

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -40,7 +40,8 @@ class Functions_Practice < MiniTest::Test
     string_1 = "Mary had a little lamb, "
     string_2 = "its fleece was white as snow"
     joined_string = join_string( string_1, string_2 )
-    assert_equal( "Mary had a little lamb, its fleece was white as snow", joined_string )
+    assert_equal( "Mary had a little lamb, its fleece was white as snow",
+      joined_string )
   end
 
   def test_add_string_as_number()
@@ -67,33 +68,33 @@ class Functions_Practice < MiniTest::Test
     first_month_string = number_to_short_month_name( 1 )
     assert_equal( "Jan", first_month_string )
   end
-
-  def test_substring__month_4()
-    fourth_month_string = number_to_short_month_name( 4 )
-    assert_equal( "Apr", fourth_month_string )
-  end
-
-  def test_substring__month_10()
-    tenth_month_string = number_to_short_month_name( 10 )
-    assert_equal( "Oct", tenth_month_string )
-  end
-
-   #Further
-
-  #Given the length of a side of a cube calculate the volume
-  def test_volume_of_cube(length)
-    volume = volume_of_cube(length)
-  end
 #
-#   #Given the radius of a sphere calculate the volume
-#   def test_volume_of_sphere()
-#     #add test code here
+#   def test_substring__month_4()
+#     fourth_month_string = number_to_short_month_name( 4 )
+#     assert_equal( "Apr", fourth_month_string )
 #   end
 #
-#   #Given a value in farenheit, convert this into celsius.
-#   def test_fahrenheit_to_celsius()
-#     #add test code here
+#   def test_substring__month_10()
+#     tenth_month_string = number_to_short_month_name( 10 )
+#     assert_equal( "Oct", tenth_month_string )
 #   end
 #
+#    #Further
 #
+#   #Given the length of a side of a cube calculate the volume
+#   def test_volume_of_cube(length)
+#     volume = volume_of_cube(length)
+#   end
+# #
+# #   #Given the radius of a sphere calculate the volume
+# #   def test_volume_of_sphere()
+# #     #add test code here
+# #   end
+# #
+# #   #Given a value in farenheit, convert this into celsius.
+# #   def test_fahrenheit_to_celsius()
+# #     #add test code here
+# #   end
+# #
+# #
 end
